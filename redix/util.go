77058ca644c024db4f2f4f2f33c11d6d8f618e80
Latermoon <lptmoon@gmail.com>
2015-11-29 23:44:41 +0800

    add redix

---
+++ redix/util.go
@@ -0,0 +1,23 @@
+package redix
+
+import (
+	"strconv"
+)
+
+var itoaCache []string
+
+func init() {
+	itoaCache = make([]string, 1000)
+	for i, count := 0, len(itoaCache); i < count; i++ {
+		itoaCache[i] = strconv.Itoa(i)
+	}
+}
+
+// itoa speed up the strconv.Itoa in small numbers
+func itoa(i int) string {
+	if i > 0 && i < len(itoaCache) {
+		return itoaCache[i]
+	} else {
+		return strconv.Itoa(i)
+	}
+}
